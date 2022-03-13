0564c148131c1d59a16c541287749276f6066f0f
小鑫同学 <zxin088@gmail.com>
2022-03-13 23:15:23 +0800

    🐎 ci:

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,11 @@ module.exports = {
   lang: "zh-CN",
   title: "IT200持续为您导航",
   description: "IT200持续为您导航",
+  base: "/it200-2022/",
   theme: "@vuepress/theme-default",
   themeConfig: {
     logo: "/images/logo.b96656d2.jpg",
     repo: "https://github.com/OSpoon/it200-2022",
-    base: "/it200-2022/",
     navbar: [
       {
         text: "IT200导航",
